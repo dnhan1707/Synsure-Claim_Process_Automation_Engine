899105850bbd9c8a9a9b01df7eb99221a42b88e8
czLad <myatzawowl@gmail.com>
2025-10-16 00:36:37 -0700

    Merging dashboard with synsure-v2

--- app/service/model_service.py
+++ app/service/model_service.py
@@ -57,7 +57,6 @@ class ModelService():
                     reasoning={"effort": "medium"},
                     text={"verbosity": "low"},
                 )
-                
                 clean_response = response.output_text.strip()
                 is_valid, result = self.validator.validate_gemini_response(clean_response)
                 
